ec126f46f43e48551e54861df4f49c21786e9e7b
Katihabwa <katicheche2001@gmail.com>
2025-04-30 14:46:41 +0200

    Création de UtilisateurService

---
+++ src/main/java/com/example/secondhand/Service/UtilisateurService.java
@@ -0,0 +1,33 @@
+package com.example.secondhand.Service;
+import com.example.secondhand.Entity.Utilisateur;
+import com.example.secondhand.Repository.UtilisateurRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+@Service
+public class UtilisateurService {
+
+    private final UtilisateurRepository utilisateurRepository;
+
+    public UtilisateurService(UtilisateurRepository utilisateurRepository) {
+        this.utilisateurRepository = utilisateurRepository;
+    }
+
+    public Utilisateur creerUtilisateur(Utilisateur utilisateur) {
+        return utilisateurRepository.save(utilisateur);
+    }
+
+    public List<Utilisateur> getTousLesUtilisateurs() {
+        return utilisateurRepository.findAll();
+    }
+
+    public Optional<Utilisateur> getUtilisateurParId(Long id) {
+        return utilisateurRepository.findById(id);
+    }
+
+    public void supprimerUtilisateur(Long id) {
+        utilisateurRepository.deleteById(id);
+    }
+    
+
+}
