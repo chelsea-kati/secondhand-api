7f3f14994ae7bfd02ea6d58bb46c807782a10a64
Katihabwa <katicheche2001@gmail.com>
2025-04-30 14:09:10 +0200

    Ajout de l'entité Utilisateur

---
+++ src/main/java/com/example/secondhand/Entity/Utilisateur.java
@@ -0,0 +1,43 @@
+package com.example.secondhand.Entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "utilisateur")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Utilisateur {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Le nom est obligatoire")
+    private String nom;
+
+    @NotBlank(message = "L'email est obligatoire")
+    @Email(message = "Email invalide")
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank(message = "Le mot de passe est obligatoire")
+    private String motDePasse;
+
+    private String telephone;
+
+    private String adresse;
+
+    private LocalDate dateInscription = LocalDate.now();
+
+    private String role = "utilisateur"; // Par défaut : "utilisateur"
+
+    private boolean actif = true; // Par défaut actif
+    
+}
