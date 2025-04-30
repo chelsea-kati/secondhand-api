9defd3f9391d26124c546680e1f03483d7b2931d
Katihabwa <katicheche2001@gmail.com>
2025-04-30 14:41:56 +0200

    Création du repository UtilisateurRepository

---
+++ src/main/java/com/example/secondhand/Repository/UtilisateurRepository.java
@@ -0,0 +1,11 @@
+package com.example.secondhand.Repository;
+import com.example.secondhand.Entity.Utilisateur;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {
+    // Tu pourras ajouter des méthodes personnalisées ici plus tard
+    Utilisateur findByEmail(String email);
+}
+
