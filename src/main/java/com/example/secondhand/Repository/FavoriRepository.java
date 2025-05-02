461ed78551033236a5364b32a145ff8a967c11db
Katihabwa <katicheche2001@gmail.com>
2025-05-02 14:02:00 +0200

    Ajout de la vérification des doublons avant ajout dans FavoriService

--- src/main/java/com/example/secondhand/Repository/FavoriRepository.java
+++ src/main/java/com/example/secondhand/Repository/FavoriRepository.java
@@ -3,8 +3,12 @@ import com.example.secondhand.Entity.Favori;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
+
 
    public interface FavoriRepository extends JpaRepository<Favori, Long> {
         List<Favori> findByUtilisateurId(Long utilisateurId);
+          // Ajout de la méthode pour vérifier l'existence d'un favori avec utilisateur_id et annonce_id
+    Optional<Favori> findByUtilisateurIdAndAnnonceId(Long utilisateurId, Long annonceId);
     
 }
--- src/main/java/com/example/secondhand/Service/FavoriService.java
+++ src/main/java/com/example/secondhand/Service/FavoriService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class FavoriService {
@@ -14,8 +15,20 @@ public class FavoriService {
     private FavoriRepository favoriRepository;
 
     public Favori ajouterFavori(Favori favori) {
-        return favoriRepository.save(favori);
+        // return favoriRepository.save(favori);
+                // Vérifier si le favori existe déjà pour cet utilisateur et cette annonce
+                Optional<Favori> existingFavori = favoriRepository
+                .findByUtilisateurIdAndAnnonceId(favori.getUtilisateur().getId(), favori.getAnnonce().getId());
+        
+        if (existingFavori.isPresent()) {
+            // Si le favori existe déjà, ne pas l'ajouter à nouveau
+            return existingFavori.get();
+        } else {
+            // Sinon, enregistrer le nouveau favori
+            return favoriRepository.save(favori);
+        }
     }
+    
 
     public List<Favori> getFavorisParUtilisateur(Long utilisateurId) {
         return favoriRepository.findByUtilisateurId(utilisateurId);
