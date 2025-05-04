faca5833851936384e40036755de2529ffa95fac
Katihabwa <katicheche2001@gmail.com>
2025-05-04 19:41:34 +0200

    Création du service MessageService

---
+++ src/main/java/com/example/secondhand/Service/MessageService.java
@@ -0,0 +1,35 @@
+package com.example.secondhand.Service;
+
+import com.example.secondhand.Entity.Message;
+import com.example.secondhand.Entity.Utilisateur;
+import com.example.secondhand.Repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+//import java.util.Optional;
+
+@Service
+
+public class MessageService {
+    @Autowired
+    private MessageRepository messageRepository;
+
+    public Message envoyerMessage(Message message) {
+        message.setDateEnvoi(new Date());
+        return messageRepository.save(message);
+    }
+
+    public List<Message> voirMessagesRecus(Utilisateur destinataire) {
+        return messageRepository.findByDestinataire(destinataire);
+    }
+
+    public List<Message> voirMessagesEnvoyes(Utilisateur expediteur) {
+        return messageRepository.findByExpediteur(expediteur);
+    }
+
+    public void supprimerMessage(Long id) {
+        messageRepository.deleteById(id);
+    }
+}
