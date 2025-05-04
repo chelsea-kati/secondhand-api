1bcb4360f5ee7f630fd0cdcd700e7cd25eeb4df2
Katihabwa <katicheche2001@gmail.com>
2025-05-04 18:55:39 +0200

    creation de l'entite Message

---
+++ src/main/java/com/example/secondhand/Entity/Message.java
@@ -0,0 +1,33 @@
+package com.example.secondhand.Entity;
+
+import jakarta.persistence.*;
+import java.util.Date;
+import lombok.*;
+
+@Entity
+@Table(name = "message")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "expediteur_id")
+    private Utilisateur expediteur;
+
+    @ManyToOne
+    @JoinColumn(name = "destinataire_id")
+    private Utilisateur destinataire;
+
+    private String contenu;
+
+    private Date dateEnvoi;
+
+    @ManyToOne
+    @JoinColumn(name = "annonce_id", nullable = true)
+    private Annonce annonce;
+}
