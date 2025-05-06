fb72bc79b8309f5c89f06b2d02b53063de759ea3
Katihabwa <katicheche2001@gmail.com>
2025-05-06 10:43:13 +0200

    Ajout du CommentaireRepository avec méthode de recherche par annonce

--- src/main/java/com/example/secondhand/Repository/CommentaireRepository.java
+++ src/main/java/com/example/secondhand/Repository/CommentaireRepository.java
@@ -1,10 +1,10 @@
 package com.example.secondhand.Repository;
 import com.example.secondhand.Entity.Commentaire;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface CommentaireRepository extends JpaRepository<Commentaire, Long> {
     List<Commentaire> findByAnnonceId(Long annonceId);
-    List<Commentaire> findByParentId(Long parentId);
 }
