0f53f18ae32a2d86764a1eb2ccc93331a9151fcf
Katihabwa <katicheche2001@gmail.com>
2025-05-07 11:15:45 +0200

    feat: finalise AuthentificationService et UtilisateurRepository pour auth JWT

---
+++ src/main/java/com/example/secondhand/Service/AuthenticationService.java
@@ -0,0 +1,52 @@
+package com.example.secondhand.Service;
+import com.example.secondhand.dto.AuthenticationRequest;
+import com.example.secondhand.dto.AuthenticationResponse;
+import com.example.secondhand.dto.RegisterRequest;
+import com.example.secondhand.Entity.Utilisateur;
+import com.example.secondhand.Enum.Role;
+import com.example.secondhand.Repository.UtilisateurRepository;
+import com.example.secondhand.Security.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Service
+@RequiredArgsConstructor
+
+public class AuthenticationService {
+    private final UtilisateurRepository utilisateurRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        Utilisateur utilisateur = new Utilisateur();
+        utilisateur.setNom(request.getNom());
+        //utilisateur.setPrenom(request.getPrenom());
+        utilisateur.setEmail(request.getEmail());
+        utilisateur.setMotDePasse(passwordEncoder.encode(request.getPassword()));
+        utilisateur.setTelephone(request.getTelephone());
+        utilisateur.setAdresse(request.getAdresse());
+        utilisateur.setRole(Role.UTILISATEUR);
+        utilisateurRepository.save(utilisateur);
+        String token = jwtService.generateToken(utilisateur.getEmail());
+        return new AuthenticationResponse(token);
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(
+                request.getEmail(),
+                request.getPassword()
+            )
+        );
+        Utilisateur utilisateur = utilisateurRepository.findByEmail(request.getEmail())
+            .orElseThrow(() -> new UsernameNotFoundException("Utilisateur non trouvé"));
+        String token = jwtService.generateToken(utilisateur.getEmail());
+        return new AuthenticationResponse(token);
+    }
+    
+}
