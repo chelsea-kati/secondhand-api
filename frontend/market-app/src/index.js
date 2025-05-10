275597bdd08bcf690082cd12a0cc75edb948ac71
Katihabwa <katicheche2001@gmail.com>
2025-05-10 12:57:05 +0200

    :creation d'index.js pour frontend

--- frontend/market-app/src/index.js
+++ frontend/market-app/src/index.js
@@ -15,4 +15,14 @@
 // // to log results (for example: reportWebVitals(console.log))
 // // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
