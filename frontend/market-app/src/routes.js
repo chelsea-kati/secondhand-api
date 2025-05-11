50bfc2251d6031567f83e345ad99cb68be331335
Katihabwa <katicheche2001@gmail.com>
2025-05-11 08:54:20 +0200

    ajout de la page CreateAnnoncePage pour publier une annonce

--- frontend/market-app/src/routes.js
+++ frontend/market-app/src/routes.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+//import React, { Component } from 'react';
+//import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import CreateAnnoncePage from './pages/CreateAnnoncePage';
 
 // const AppRoutes = () => {
 //   return (
@@ -19,6 +20,7 @@ const routes = [
     { path: '/', Component: HomePage },
     { path: '/login', Component: LoginPage },
     {path: '/register', Component: RegisterPage},
+    { path: '/AddAnnonce', Component: CreateAnnoncePage },
   ];
   
   export default routes;
