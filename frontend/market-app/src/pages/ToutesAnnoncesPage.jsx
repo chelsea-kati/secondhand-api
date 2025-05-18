a73444bf1a741db560c1b6d2e052106e7877e1b1
Katihabwa <katicheche2001@gmail.com>
2025-05-18 12:53:46 +0200

    feat: ajout de\u css pour la page ListeUtilisateursPage pour l’admin

--- frontend/market-app/src/pages/ToutesAnnoncesPage.jsx
+++ frontend/market-app/src/pages/ToutesAnnoncesPage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import './ToutesAnnoncesPage.css;'
 
 const ToutesAnnoncesPage = () => {
   const [annonces, setAnnonces] = useState([]);
